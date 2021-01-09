4f6c126564c9085704804441d0aa5b94de427bf5
SH.H <e2xist@gmail.com>
2021-01-09 22:05:30 +0900

    preset 에 파일명 추가.

--- sample.json.js
+++ sample.json.js
@@ -3,6 +3,7 @@ var sample = {
 		"name" : "샘플",
 		"foregroundSvg" : "./sample/accessibility_new-24px.svg",
 		"backgroundSvg" : "./sample/background-round.svg",
+		"filename" : "svg-to-png-sample-{size}px.png",
 		"sizes" : [48, 72, 96, 144, 192]
 	}
 }
